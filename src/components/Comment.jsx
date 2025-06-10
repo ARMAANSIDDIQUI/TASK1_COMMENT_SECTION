1bd3c43560472c4544253973a5665f430ff2ff30
Armaan Siddiqui <148369512+ARMAANSIDDIQUI@users.noreply.github.com>
2025-06-10 18:43:15 +0530

    Update Comment.jsx

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,11 +3,9 @@ export function Comment({ comment, level = 0 }) {
     const [showReplies, setShowReplies] = useState(false);
     const hasReplies = comment.replies && comment.replies.length > 0;
     return (
-        <div className={`relative border border-gray-700 rounded-sm p-8 mt-4 ml-[${level * 4}]`}>
-            {level >= 0 && (
+        <div className={`relative border border-gray-700 rounded-sm p-8 mt-4`}>
                 <div className="absolute left-[-12px] top-0 bottom-0 w-1 ml-8 mt-2 mb-2 bg-gray-200 hover:bg-blue-400 cursor-pointer" 
                 onClick={() => setShowReplies(!showReplies)}/>
-            )}
             <div className="flex items-start space-x-3">
                 <div className="w-7 h-7 rounded-full bg-gray-600 flex-shrink-0 overflow-hidden">
                     <img src={comment.image} alt="user_img" className="w-full h-full object-cover" />
@@ -30,8 +28,8 @@ export function Comment({ comment, level = 0 }) {
                             </button>
                         </div>
                     )}
-                    {showReplies && comment.replies.map((reply, index) => (
-                            <Comment key={index} comment={reply} level={level + 1} />
+                    {showReplies && comment.replies.map((reply) => (
+                            <Comment comment={reply} level={level + 1} />
                         ))}
                 </div>
             </div>
